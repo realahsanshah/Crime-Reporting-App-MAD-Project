80d1b2240923ce57a640fe71797ce9eb74753e5f
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-12-20 00:11:27 +0500

    added recycler view

--- app/src/main/java/com/example/crimereportingapp/CrimeListActivity.java
+++ app/src/main/java/com/example/crimereportingapp/CrimeListActivity.java
@@ -1,10 +1,7 @@
 package com.example.crimereportingapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
-import android.os.Bundle;
 
 public class CrimeListActivity extends SingleFragmentActivity {
 
--- app/src/main/java/com/example/crimereportingapp/CrimeListFragment.java
+++ app/src/main/java/com/example/crimereportingapp/CrimeListFragment.java
@@ -1,6 +1,34 @@
 package com.example.crimereportingapp;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class CrimeListFragment extends Fragment {
+
+    private RecyclerView crimesRecyclerView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        crimesRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        crimesRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+
+    }
 }
