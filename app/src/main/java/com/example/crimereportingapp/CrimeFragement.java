55dc97c67bf1fb6e2bc5f3721c408f261a919aeb
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-12-19 21:20:52 +0500

    added some listener

--- app/src/main/java/com/example/crimereportingapp/CrimeFragement.java
+++ app/src/main/java/com/example/crimereportingapp/CrimeFragement.java
@@ -1,11 +1,15 @@
 package com.example.crimereportingapp;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
@@ -17,10 +21,13 @@ public class CrimeFragement extends Fragment {
     private Button dateButton;
     private CheckBox solvedCheckBox;
     private EditText crimeTitleText;
+    private Crime crime;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        crime=new Crime();
     }
 
     @Nullable
@@ -31,8 +38,34 @@ public class CrimeFragement extends Fragment {
         View view=inflater.inflate(R.layout.fragement_crime,container,false);
 
         dateButton=(Button)view.findViewById(R.id.crime_button);
+        dateButton.setText(crime.getDate().toString());
+        dateButton.setEnabled(false);
         solvedCheckBox=(CheckBox)view.findViewById(R.id.crime_solved);
+        solvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                crime.setSolved(isChecked);
+            }
+        });
         crimeTitleText=(EditText)view.findViewById(R.id.crime_title);
+
+        crimeTitleText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.i("s",s.toString());
+                crime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         return view;
 
