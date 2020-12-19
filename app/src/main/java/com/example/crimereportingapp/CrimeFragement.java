c38f6c697374a92e1373cfcea6c9f621d2e8241e
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-12-19 21:04:50 +0500

    Initialized Fragment's values

--- app/src/main/java/com/example/crimereportingapp/CrimeFragement.java
+++ app/src/main/java/com/example/crimereportingapp/CrimeFragement.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class CrimeFragement extends Fragment {
+
+    private Button dateButton;
+    private CheckBox solvedCheckBox;
+    private EditText crimeTitleText;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +26,15 @@ public class CrimeFragement extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+//        super.onCreateView(inflater, container, savedInstanceState);
+
+        View view=inflater.inflate(R.layout.fragement_crime,container,false);
+
+        dateButton=(Button)view.findViewById(R.id.crime_button);
+        solvedCheckBox=(CheckBox)view.findViewById(R.id.crime_solved);
+        crimeTitleText=(EditText)view.findViewById(R.id.crime_title); 
+
+        return view;
+
     }
 }
