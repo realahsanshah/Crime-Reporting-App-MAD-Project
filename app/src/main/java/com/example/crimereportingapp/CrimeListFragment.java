305d0d6dd6f53261dcae2791c46c9a6a11d3af20
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-12-20 00:45:52 +0500

    rendered the views

--- app/src/main/java/com/example/crimereportingapp/CrimeListFragment.java
+++ app/src/main/java/com/example/crimereportingapp/CrimeListFragment.java
@@ -1,9 +1,11 @@
 package com.example.crimereportingapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,17 +13,60 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class CrimeListFragment extends Fragment {
 
     private class CrimeHolder extends RecyclerView.ViewHolder{
-        public CrimeHolder(LayoutInflater inflater,ViewGroup parent){
-            super(inflater.inflate(R.layout.fragment_crime_list,parent,false));
+        private TextView titleTextView;
+        private TextView dateTextView;
 
+        private Crime mCrime;
+
+        public CrimeHolder(LayoutInflater inflater,ViewGroup parent){
+            super(inflater.inflate(R.layout.list_item_crime,parent,false));
+
+            titleTextView=(TextView)itemView.findViewById(R.id.crime_item_title);
+            dateTextView=(TextView)itemView.findViewById(R.id.crime_item_date);
+        }
+
+        public void bind(Crime crime){
+            this.mCrime=crime;
+            titleTextView.setText(mCrime.getTitle());
+            dateTextView.setText(mCrime.getDate().toString());
+        }
+    }
+
+    public class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>{
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> mCrimes) {
+            this.mCrimes = mCrimes;
+        }
+
+        @NonNull
+        @Override
+        public CrimeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater=LayoutInflater.from(getActivity());
+
+            return new CrimeHolder(inflater,parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CrimeHolder holder, int position) {
+            Crime crime=mCrimes.get(position);
+            holder.bind(crime);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
         }
     }
 
 
     private RecyclerView crimesRecyclerView;
+    private CrimeAdapter crimeAdapter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,7 +81,18 @@ public class CrimeListFragment extends Fragment {
         crimesRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
         crimesRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        updateUI();
+
         return view;
 
+    }
+
+    private void updateUI() {
+        CrimeLab crimeLab=CrimeLab.get(getActivity());
+        List<Crime> crimes=crimeLab.getCrimes();
+
+        crimeAdapter=new CrimeAdapter(crimes);
+
+        crimesRecyclerView.setAdapter(crimeAdapter);
     }
 }
