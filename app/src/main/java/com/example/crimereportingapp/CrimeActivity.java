4f9b431de6b47c43a99aacec5d84ae5c18495075
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-12-19 20:02:33 +0500

    Created Fragment Layout

---
+++ app/src/main/java/com/example/crimereportingapp/CrimeActivity.java
@@ -0,0 +1,14 @@
+package com.example.crimereportingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
