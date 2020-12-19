c654d2e8638597dc05e44570e4af4ba3d2dd4849
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-12-19 21:28:33 +0500

    populated fragment

--- app/src/main/java/com/example/crimereportingapp/CrimeActivity.java
+++ app/src/main/java/com/example/crimereportingapp/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.example.crimereportingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,14 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+
+        Fragment fragment=fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if(fragment==null){
+            fragment=new CrimeFragement();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container,fragment).commit();
+        }
     }
 }
