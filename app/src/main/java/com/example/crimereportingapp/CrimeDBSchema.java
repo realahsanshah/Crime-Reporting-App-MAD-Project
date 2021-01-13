fb5c3af4152b0b8ee928ea8134a99bbc8416c82c
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2021-01-13 14:02:29 +0500

    added schema

---
+++ app/src/main/java/com/example/crimereportingapp/CrimeDBSchema.java
@@ -0,0 +1,18 @@
+package com.example.crimereportingapp;
+
+public class CrimeDBSchema {
+
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        public static final class Columns {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+            public static final String POLICEREQUIRED = "policerequired";
+
+
+        }
+    }
+}
